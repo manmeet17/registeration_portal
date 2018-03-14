83bcb35b9e02ed65e29bc0c1663cabd75bf49925
Manmeet Tarun <meetutarun16@gmail.com>
2018-03-14 08:30:52 +0530

    IOS fix

--- js/register.js
+++ js/register.js
@@ -28,15 +28,18 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
   });
 
 function validate(con,callback){
-    var name = $('input[name=fname]');
-    var email= $('input[name=email]');
-    var room= $('input[name=room');
-    var regNo = $('input[name=regNo]');
-    var mobile = $('input[name=mobile]');
-    var github = $('input[name=github]');
-    var linkedin= $('input[name=linkedin]');
-    var skills= $('input[name=skills]');
-    if(name.val().length==0 || email.val().length==0 || mobile.val().length!=10 || room.val().length==0 || regNo.val().length!=9 || skills.val().length==0 ){
+    var fields={
+    name : $('input[name=fname]').val(),
+    email: $('input[name=email]').val(),
+    room : $('input[name=room').val(),
+    regNo : $('input[name=regNo]').val(),
+    mobile : $('input[name=mobile]').val(),
+    github : $('input[name=github]').val(),
+    linkedin: $('input[name=linkedin]').val(),
+    skills: $('input[name=skills]').val(),
+    };
+    console.log(fields);
+    if( fields.name.length==0 || fields.email.length==0 || fields.mobile.length!=10 || fields.room.length==0 || fields.regNo.length!=9 || fields.skills.length==0 ){
         swal({
             title: "Failed",
             text: "Make sure you have filled all the fields correctly",
@@ -46,35 +49,36 @@ function validate(con,callback){
     else{
         // console.log("Called");
         con=true;
-        callback();
+        callback(fields);
     }
 }
 
-function request(){
+function request(fields){
     $.ajax({
         url: 'https://hackoverflow-api.herokuapp.com/new-student',
         type: 'POST',
         dataType: 'json',
         contentType: 'application/json',
-        data: JSON.stringify({
-            name: $('input[name=fname]').val(),
-            email: $('input[name=email]').val(),
-            room: $('input[name=room').val(),
-            regNo: $('input[name=regNo]').val(),
-            mobile: $('input[name=mobile]').val(),
-            github: $('input[name=github]').val(),
-            linkedin: $('input[name=linkedin]').val(),
-            skills: $('input[name=skills]').val()
-        }),
+        data: JSON.stringify(fields),
         success: function(res){
-            swal({
-                title: "Success",
-                text: "You have registered for hackoverflow",
-                icon: "success"
-            }).then((res) =>{
-                window.location.href="./index.html";
-                // window.location.href="/";
-            });
+            console.log(res);
+            if(res.status==200){
+                swal({
+                    title: "Success",
+                    text: "You have registered for hackoverflow",
+                    icon: "success"
+                }).then((res) =>{
+                    window.location.href="./index.html";
+                    // window.location.href="/";
+                });
+            }
+            else{
+                swal({
+                    title: "Failed",
+                    text: fields.name+"has already registered for Hackoverflow",
+                    icon: "error"
+                })
+            }
         }
     });
 }
